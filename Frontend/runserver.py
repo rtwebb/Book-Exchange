e78a5dc54a6d57882f7d572eac154d7c097224db
Robert <rtwebb@princeton.ed>
2020-10-13 16:20:57 -0400

    runserver

--- Frontend/runserver.py
+++ Frontend/runserver.py
@@ -14,11 +14,12 @@ def main(argv):
         print('Usage: ' + argv[0] + ' port', file=stderr)
         exit(1)
      
-        try:
-            port = int(argv[1])
-        except:
-            print('Port must be an integer.', file=stderr)
-            exit(1)
+    try:
+        port = int(argv[1])
+
+    except:
+        print('Port must be an integer.', file=stderr)
+        exit(1)
          
     app.run(host='0.0.0.0', port=port, debug=True)
 
