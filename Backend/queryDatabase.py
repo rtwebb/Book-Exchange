5a71447d8467c42a014b409604a98c937905ccf3
Robert <rtwebb@princeton.ed>
2020-10-19 20:25:01 -0400

    databse connection

---
+++ Backend/queryDatabase.py
@@ -0,0 +1,31 @@
+#---------------------------------------
+# Querydatabse
+
+# By: Emmandra, Tiana, Toussaint
+#----------------------------------------
+
+Class Querydatabase():
+
+    def __init__(self):
+        self._.connection = None
+    
+    
+    def connect(self):
+        
+
+    def disconnect(self):
+
+
+
+    def add(self):
+
+
+
+    def remove(self):
+
+    
+
+    def search(self):
+
+
+    
