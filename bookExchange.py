146ea189c81e46362079932cf14c37c56fe9c693
Robert <rtwebb@princeton.ed>
2020-10-21 14:20:57 -0400

    comments in flask and two new small pages

--- bookExchange.py
+++ bookExchange.py
@@ -16,6 +16,10 @@ app = Flask(__name__, template_folder='template')
 @app.route('/homePage', methods=['GET'])
 def homePageTemplate():
 
+    # need to get recently listed books to show
+    # once get object send to homePage
+    # each book needs to link to buyerPage with more information
+    # set cookies on search query to follow through searchResults and buyerPage
     
     html = render_template('homePage.html')
                           
@@ -27,17 +31,22 @@ def homePageTemplate():
 @app.route('/searchResults', methods=['GET'])
 def searchResultsTemplate():
 
+    # need to get results based on search query
+    # once get object send to searchResults
+    # each book needs to link to buyerPage with more information
+    # set cookies on search query to follow through searchResults and buyerPage
+
     isbn = request.args.get('query')
-    result = []
+    results = []
     try: 
         database = querydatabse()
         database.connect()
-        result = database.search(isbn)
+        results = database.search(isbn)
         
     except Exception as e:
         print(argv[0] + ": " + str(e), file=stderr)   
     
-    html = render_template('searchResults.html', results=result)
+    html = render_template('searchResults.html', results=results)
                           
     response = make_response(html)
     return response
@@ -48,7 +57,10 @@ def searchResultsTemplate():
 @app.route('/sellerPage', methods=['GET'])
 def sellerPageTemplate():
 
-    # include the information that needs to be filled in dependent on the template
+    # pulls down information and adds to database if user says yes everything is correct
+      # to check if correct use confirmationPage (is this information right?)
+         # if yes generates sucess page
+         # if no stay go back to sellerPage (with saved information) and they can fix 
 
     html = render_template('sellerPage.html')
                           
@@ -60,9 +72,12 @@ def sellerPageTemplate():
 
 @app.route('/buyerPage', methods=['GET'])
 def buyerPageTemplate():
-
-
-    # include the information that needs to be filled in dependent on the template
+    # buyerPage needs link back to home page 
+    
+    # If user makes a bid
+       # check to make sure if they are sure about the amount
+          # if it is correct show success page and have a link to go back to homePage
+          # if no stay on buyer page 
 
     html = render_template('buyerPage.html')
                           
@@ -72,10 +87,24 @@ def buyerPageTemplate():
 
 #----------------------------------------------------------------------
     
-@app.route('/aboutUs', methods=['GET'])
-def aboutPageTemplate():
+@app.route('/profilePage', methods=['GET'])
+def profilePageTemplate():
 
-    # include the information that needs to be filled in dependent on the template
+    # query database for the given user
+      # get books they are selling
+         # if none set object to none, else pass along
+      # get books that they have bid on 
+         # if none set object to none, else pass along 
+    html = render_template('profilePage.html')
+                          
+    response = make_response(html)
+    return response
+
+#----------------------------------------------------------------------
+    
+@app.route('/aboutUs', methods=['GET'])
+def aboutUsTemplate():
+
 
     html = render_template('aboutUs.html')
                           
