aab53e9a92e087507f961533fe7ce314f3e74cb5
Tiana Fitzgerald <tianaf@princeton.edu>
2020-10-27 20:33:51 -0400

    name standardization in homePage.html vs. bookExchange.py so database does not add None for course title, commented out image code in queryDatabase.py add method for functionality

--- bookExchange.py
+++ bookExchange.py
@@ -1,23 +1,23 @@
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 # bookExchange.py
-# book-exchange flask file, that works as web-frameowkr
+# book-exchange flask file, that works as web-framework
 # Author: Toussaint
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 from sys import stderr, argv
 from flask import Flask, request, make_response, redirect, url_for
 from flask import render_template
 from queryDatabase import QueryDatabase
 from datetime import datetime
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 app = Flask(__name__, template_folder='template')
 
-#-----------------------------------------------------------------------
+
+# -----------------------------------------------------------------------
 @app.route('/', methods=['GET'])
 @app.route('/homePage', methods=['GET'])
 def homePageTemplate():
-
     # need to get recently listed books to show
     try:
         database = QueryDatabase()
@@ -29,48 +29,46 @@ def homePageTemplate():
         errorMsg = 'An error occured please contact email at bottom of the screen'
 
     # set cookies on search query to follow through searchResults and buyerPage
-        
-    html = render_template('homePage.html', result=result, errorMsg=errorMsg)                      
+
+    html = render_template('homePage.html', result=result, errorMsg=errorMsg)
     response = make_response(html)
     return response
 
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 @app.route('/searchResults', methods=['GET'])
 def searchResultsTemplate():
-
     # need to get results based on search query
     # once get object send to searchResults
     # each book needs to link to buyerPage with more information
     # set cookies on search query to follow through searchResults and buyerPage
 
     # how to check what drop-down was selected
-    
+
     isbn = request.args.get('query')
     results = []
-    try: 
+    try:
         database = QueryDatabase()
         database.connect()
         results = database.search(isbn)
-        
+
     except Exception as e:
-        print(argv[0] + ": " + str(e), file=stderr)   
-    
+        print(argv[0] + ": " + str(e), file=stderr)
+
     html = render_template('searchResults.html', results=results)
-                          
+
     response = make_response(html)
     return response
 
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 @app.route('/sellerPage', methods=['GET'])
 def sellerPageTemplate():
-
     # pulls down information and adds to database if user says yes everything is correct
-      # to check if correct use confirmationPage (is this information right?)
-         # if yes generates sucess page
-         # if no stay go back to sellerPage (with saved information) and they can fix 
+    # to check if correct use confirmationPage (is this information right?)
+    # if yes generates sucess page
+    # if no stay go back to sellerPage (with saved information) and they can fix
 
     isbn = request.args.get('isbn')
     title = request.args.get('title')
@@ -83,13 +81,13 @@ def sellerPageTemplate():
     # condition = request.args.get('condition')
     author = request.args.get('author')
     crsnum = request.args.get('crsnum')
-    crsname = request.args.get('name')
+    crsname = request.args.get('crsname')
     time = datetime.now()
     listTime = time.strftime("%H:%M:%S")
     # sellerID = pull from CAS somehow
 
     # confirmation JS stuff    
-    
+
     # passing to database
     try:
         database = QueryDatabase()
@@ -101,50 +99,48 @@ def sellerPageTemplate():
 
     # when sending to profile page have a "succesfull" message display
     html = render_template('sellerPage.html')
-                          
+
     response = make_response(html)
     return response
 
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 @app.route('/buyerPage', methods=['GET'])
 def buyerPageTemplate():
     # buyerPage needs link back to home page 
-    
+
     # If user makes a bid
-       # check to make sure if they are sure about the amount
-          # if it is correct show success page and have a link to go back to homePage
-          # if no stay on buyer page 
+    # check to make sure if they are sure about the amount
+    # if it is correct show success page and have a link to go back to homePage
+    # if no stay on buyer page
 
     html = render_template('buyerPage.html')
-                          
+
     response = make_response(html)
     return response
 
 
-#----------------------------------------------------------------------
-    
+# ----------------------------------------------------------------------
+
 @app.route('/profilePage', methods=['GET'])
 def profilePageTemplate():
-
     # query database for the given user
-      # get books they are selling
-         # if none set object to none, else pass along
-      # get books that they have bid on 
-         # if none set object to none, else pass along 
+    # get books they are selling
+    # if none set object to none, else pass along
+    # get books that they have bid on
+    # if none set object to none, else pass along
     html = render_template('profilePage.html')
-                          
+
     response = make_response(html)
     return response
 
-#----------------------------------------------------------------------
-    
+
+# ----------------------------------------------------------------------
+
 @app.route('/aboutUs', methods=['GET'])
 def aboutUsTemplate():
-
-
     html = render_template('aboutUs.html')
-                          
+
     response = make_response(html)
     return response
--- queryDatabase.py
+++ queryDatabase.py
@@ -14,7 +14,8 @@ class QueryDatabase:
 
     def __init__(self):
         self._connection = None
-#----------------------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------------------
     def connect(self):
         DATABASE_URI = 'postgres://vjlbayumjwpewg:19bf7b1ddf47645b85ddd2a53327548' \
                        'f856e138ec4104be1b99df2f432df9f85@ec2-23-23-36-227.compute-' \
@@ -22,7 +23,8 @@ class QueryDatabase:
         engine = create_engine(DATABASE_URI)
         Session = sessionmaker(bind=engine)
         self._connection = Session()
-#----------------------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------------------
     def disconnect(self):
         self._connection = None
 
@@ -32,7 +34,7 @@ class QueryDatabase:
         if book is not None:
             book.quantity += 1
             self._connection.commit()
-        
+
         else:
             book = Books(isbn=isbn, title=title, quantity=1)
             authorlist = []
@@ -46,98 +48,101 @@ class QueryDatabase:
             course = Courses(isbn=isbn, number=coursenumber, title=coursetitle)
             self._connection.add(course)
             self._connection.commit()
-            
-        listing = Listings(sellerID=sellerID, isbn=isbn, 
-                           condition=condition, minPrice=minPrice, 
+
+        listing = Listings(sellerID=sellerID, isbn=isbn,
+                           condition=condition, minPrice=minPrice,
                            buyNow=buyNow, listTime=listTime)
         self._connection.add(listing)
         self._connection.commit()
 
-        image = Images(sellerID=sellerID, isbn=isbn, url=url)
-        self._connection.add(image)
-        self._connection.commit()
-#-----------------------------------------------------------------------------                
+        # image = Images(sellerID=sellerID, isbn=isbn, url=url)
+        # self._connection.add(image)
+        # self._connection.commit()
+
+    # -----------------------------------------------------------------------------
     def remove(self, isbn, sellerID):
 
-        #Deleting from Listings Table
-        listObj = Listings.query.filter(Listings.isbn==isbn) .\
+        # Deleting from Listings Table
+        listObj = Listings.query.filter(Listings.isbn == isbn). \
             filter(Listings.sellerID == sellerID)
-        #listObj = seld._connection.query(Listings).filter(Listings.isbn==isbn) .\
-            #filter(Listings.sellerID == sellerID)
+        # listObj = seld._connection.query(Listings).filter(Listings.isbn==isbn) .\
+        # filter(Listings.sellerID == sellerID)
         self._connection.delete(listObj)
 
-        #Deleting from Images Table
-        imgObj = Images.query.filter(Images.isbn==isbn) .\
+        # Deleting from Images Table
+        imgObj = Images.query.filter(Images.isbn == isbn). \
             filter(Images.sellerID == sellerID)
-        #imgObj = self._connection.query(Images).filter(Images.isbn==isbn) .\
-            #filter(Images.sellerID == sellerID)
+        # imgObj = self._connection.query(Images).filter(Images.isbn==isbn) .\
+        # filter(Images.sellerID == sellerID)
         self._connecton.delete(imgObj)
 
-        #Deleting from Bids Table
-        bidObj = Bids.query.filter(Bids.isbn==isbn) .\
+        # Deleting from Bids Table
+        bidObj = Bids.query.filter(Bids.isbn == isbn). \
             filter(Bids.sellerID == sellerID)
-        #bidObj = self._connection.query(Bids).filter(Bids.isbn==isbn) .\
-            #filter(Bids.sellerID == sellerID)
+        # bidObj = self._connection.query(Bids).filter(Bids.isbn==isbn) .\
+        # filter(Bids.sellerID == sellerID)
         self._connecton.delete(bidObj)
 
-        #Deleting from Books Table & Authors Table if necessary
-        bookObj = Books.query.filter(Books.isbn==isbn)
-        #bookObj = self._connection.query(Books).filter(Books.isbn==isbn)
+        # Deleting from Books Table & Authors Table if necessary
+        bookObj = Books.query.filter(Books.isbn == isbn)
+        # bookObj = self._connection.query(Books).filter(Books.isbn==isbn)
         if bookObj.quantity > 1:
-            num = Books.query.filter(Books.isbn==isbn).update({Books.quantity : Books.quantity - 1}) 
-            #bookObj.quantity -= 1
+            num = Books.query.filter(Books.isbn == isbn).update({Books.quantity: Books.quantity - 1})
+            # bookObj.quantity -= 1
 
-        #the last book with this isbn was so delete the row
+        # the last book with this isbn was so delete the row
         else:
 
-            authObj = Authors.query.filter(Authors.isbn==isbn)
-            #authObj = self._connection.query(Authors).filter(Authors.isbn==isbn)
-            courseObj = Courses.query.filter(Courses.isbn==isbn)
-            #courseObj = self._connection.query(Courses).filter(Courses.isbn==isbn)
+            authObj = Authors.query.filter(Authors.isbn == isbn)
+            # authObj = self._connection.query(Authors).filter(Authors.isbn==isbn)
+            courseObj = Courses.query.filter(Courses.isbn == isbn)
+            # courseObj = self._connection.query(Courses).filter(Courses.isbn==isbn)
             self._connection.delete(authObj)
 
         self._connection.delete(bookObj)
-            
+
         self._connection.commit()
-#----------------------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------------------
     def search(self, signal, query):
         # signal tells me what kind of query it is: book title, isbn, course, etc
         result = []
         if signal == 1:  # if query is by isbn
-            found = self._connection.query(Books, Courses, Listings).\
-                filter(Listings.isbn == query).\
-                filter(Courses.isbn == Listings.isbn).\
-                filter(Books.isbn == Listings.isbn).\
+            found = self._connection.query(Books, Courses, Listings). \
+                filter(Listings.isbn == query). \
+                filter(Courses.isbn == Listings.isbn). \
+                filter(Books.isbn == Listings.isbn). \
                 order_by(Listings.listTime).all()
         elif signal == 2:  # query is a book title
-            found = self._connection.query(Books, Courses, Listings).\
-                filter(Listings.isbn == Books.isbn).\
-                filter(Books.title.like(query)).\
-                filter(Courses.isbn == Books.isbn).\
+            found = self._connection.query(Books, Courses, Listings). \
+                filter(Listings.isbn == Books.isbn). \
+                filter(Books.title.like(query)). \
+                filter(Courses.isbn == Books.isbn). \
                 order_by(Listings.listTime).all()
         elif signal == 3:  # query is a coursenum
-            found = self._connection.query(Books, Courses, Listings).\
-                filter(Listings.isbn == Courses.isbn).\
-                filter(Courses.number.like(query)).\
-                filter(Books.isbn == Courses.isbn).\
+            found = self._connection.query(Books, Courses, Listings). \
+                filter(Listings.isbn == Courses.isbn). \
+                filter(Courses.number.like(query)). \
+                filter(Books.isbn == Courses.isbn). \
                 order_by(Listings.listTime).all()
         else:  # search by course title
-            found = self._connection.query(Books, Courses, Listings).\
-                filter(Listings.isbn == Books.isbn).\
-                filter(Books.isbn == Courses.isbn).\
-                filter(Courses.title.like(query)).\
+            found = self._connection.query(Books, Courses, Listings). \
+                filter(Listings.isbn == Books.isbn). \
+                filter(Books.isbn == Courses.isbn). \
+                filter(Courses.title.like(query)). \
                 order_by(Listings.listTime).all()
         for book, course, listing in found:
             result.append((book.title, course.number, course.title, listing.minPrice))
         return result
 
         # have to figure out all of the search options we will have
-#----------------------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------------------
     def homeRecents(self):
         result = []
-        found = self._connection.query(Books, Courses, Listings).\
-            filter(Listings.isbn == Books.isbn).\
-            filter(Listings.isbn == Courses.isbn).\
+        found = self._connection.query(Books, Courses, Listings). \
+            filter(Listings.isbn == Books.isbn). \
+            filter(Listings.isbn == Courses.isbn). \
             order_by(Listings.listTime).all()
         for book, course, listing in found:
             result.append((book.title, course.number, course.title, listing.minPrice))
