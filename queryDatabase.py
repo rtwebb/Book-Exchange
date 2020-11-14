3f74a2dddeae21e976b64136b5f444add34debbb
Vedant <vdhopte@princeton.edu>
2020-11-14 00:14:50 -0500

    profile page

--- queryDatabase.py
+++ queryDatabase.py
@@ -102,7 +102,7 @@ class QueryDatabase:
         print(uniqueID)
         try:
             results = self._connection.query(Bids, Listings). \
-                filter(Bids.listingID == uniqueID). \
+                filter(Bids.listingID.contains(uniqueID)). \
                 filter(Bids.buyerID.contains(buyerID)). \
                 filter(Listings.uniqueID == Bids.listingID).all()
             for bid, listing in results:
